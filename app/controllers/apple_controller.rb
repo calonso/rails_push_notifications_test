0fea2e278be9b8605f06c1f346849b4d74329a31
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-05-20 20:49:56 +0100

    added apns

--- app/controllers/apple_controller.rb
+++ app/controllers/apple_controller.rb
@@ -6,7 +6,7 @@ class AppleController < ApplicationController
     app = RailsPushNotifications::APNSApp.new
     app.apns_dev_cert = params[:cert]
     app.apns_prod_cert = params[:cert]
-    app.sandbox_mode = params[:sandbox_mode]
+    app.sandbox_mode = !!params[:sandbox_mode]
 
     if app.save
       notif = app.notifications.build(
@@ -17,13 +17,14 @@ class AppleController < ApplicationController
       if notif.save
         app.push_notifications
         notif.reload
-        flash[:notice] = notif.results.inspect
+        flash.now[:notice] = "Notification successfully pushed through!. Results #{notif.results.success} succeded, #{notif.results.failed} failed"
       else
-        flash[:error] = notif.errors.full_messages
+        flash.now[:error] = notif.errors.full_messages
       end
     else
-      flash[:error] = app.errors.full_messages
+      flash.now[:error] = app.errors.full_messages
     end
+
     render :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518192518) do
+ActiveRecord::Schema.define(version: 20150520192657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20150518192518) do
     t.string   "app_type"
     t.text     "data"
     t.text     "results"
+    t.integer  "success"
+    t.integer  "failed"
     t.boolean  "sent",         default: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
