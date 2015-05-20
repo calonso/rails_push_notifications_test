dd5203604aed8036e797ee6149fedcf8261bd766
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-05-20 21:15:48 +0100

    added android

---
+++ app/controllers/android_controller.rb
@@ -0,0 +1,30 @@
+class AndroidController < ApplicationController
+  def index
+  end
+
+  def create
+    app = Rpn::GCMApp.new
+    app.gcm_key = params[:gcm_api_key]
+    app.save
+
+    if app.save
+      notif = app.notifications.build(
+        destinations: [params[:destination]],
+        data: { text: params[:message] }
+      )
+
+      if notif.save
+        app.push_notifications
+        notif.reload
+        flash[:notice] = "Notification successfully pushed through!. Results #{notif.results.success} succeded, #{notif.results.failed} failed"
+        redirect_to :apple_index
+      else
+        flash.now[:error] = notif.errors.full_messages
+        render :index
+      end
+    else
+      flash.now[:error] = app.errors.full_messages
+      render :index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :apple, only: [:index, :create]
+  resources :android, only: [:index, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
